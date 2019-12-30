2307629e24b35c404360117539055080c23f978e
lpgoulart <leonardo@roffor.com.br>
2019-12-30 11:42:57 -0300

    add folder feature

--- src/app/pages/home/addFolder/addFolder.component.ts
+++ src/app/pages/home/addFolder/addFolder.component.ts
@@ -1,26 +1,45 @@
 import { Component, OnInit } from "@angular/core";
 import { TextField } from "tns-core-modules/ui/text-field";
 import { ListService } from "~/app/service/list.service";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'add-folder-component',
-    templateUrl: "./addFolder.component.html"
+    templateUrl: "./addFolder.component.html",
+    styleUrls: ['./addFolder.component.css']
 })
 
 export class AddFolderComponent implements OnInit {
     
     title = ""
+    isCreated: boolean
+    invalidInput: boolean
     
-    constructor(private listService: ListService){}
+    constructor(private listService: ListService, private _router: Router){}
 
-    ngOnInit(){}
+    ngOnInit(){
+        this.isCreated = false;
+        this.invalidInput = false;
+        this.title = "Nova Pasta"
+    }
 
     update(args) {
         let textField = <TextField>args.object;
         this.title = textField.text;
+        this.invalidInput = false;
     }
 
     saveFolder() {
-        this.listService.add(this.title, Date.now())
+        this.listService.add(this.title, Date.now());
+
+        if( this.title != "" || this.title != "" ){
+            setTimeout(() => {
+                this._router.navigate(['/home'])
+            }, 1000);
+            this.isCreated = true
+        }
+        else {
+            this.invalidInput = true;
+        }
     }
 }
